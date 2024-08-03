ac9454b0746f4e007a3bef8e912a526a170bf01e
catsmoker <119059457+catsmoker@users.noreply.github.com>
2024-08-03 03:49:57 +0100

    Update cs_script.cmd

--- cs_script.cmd
+++ cs_script.cmd
@@ -1,9 +1,22 @@
 @echo off
 setlocal EnableDelayedExpansion
 
+:: Check if the script is running with administrative privileges
+net session >nul 2>&1
+if %errorLevel% neq 0 (
+    :: The script is not running as admin, restart with elevation
+    echo Requesting administrative privileges...
+    set "batchPath=%~f0"
+    :: Start a new Command Prompt with elevated privileges
+    powershell -Command "Start-Process cmd -ArgumentList '/c \"%batchPath%\"' -Verb RunAs"
+	echo Script is running with administrative privileges.
+    goto menu
+)
+goto menu
+
 ::============================================================================
 ::
-::   cs Script v1.2 (catsmoker) 
+::   cs Script v1.3 (catsmoker)
 ::
 ::   Homepages: https://github.com/catsmoker/cs_script.bat
 ::   
@@ -17,19 +30,23 @@ setlocal EnableDelayedExpansion
 
 :menu
 cls
+echo                                               cs Script v1.2 (by catsmoker) https://catsmoker.github.io
+echo                                                                run as administrator
 echo Select an option:
 echo 1. Scan and Fix Windows
 echo 2. Download Specific Applications
-echo 3. Activate windows
-echo 4. Download Atlas OS Playbook and ame wizard
+echo 3. Activate Windows
+echo 4. Download Atlas OS Playbook and AME Wizard
 echo 5. Exit
 
-set /p choice=Enter your choice (1-5):
+powershell -Command "$null = New-Item -Path ([System.IO.Path]::Combine([System.Environment]::GetFolderPath('Desktop'), 'CS Downloads')) -ItemType Directory -ErrorAction SilentlyContinue"
+
+set /p choice=Enter your choice (1-5): 
 
 if "%choice%"=="1" goto scan_fix_windows
 if "%choice%"=="2" goto download_apps
 if "%choice%"=="3" goto activate_windows
-if "%choice%"=="4" goto playbook_and_ame
+if "%choice%"=="4" goto ame_playbook
 if "%choice%"=="5" goto exit_script
 echo Invalid choice. Please enter a number between 1 and 5.
 goto menu
@@ -39,16 +56,16 @@ cls
 echo Scanning and fixing Windows...
 sfc /scannow
 if %errorlevel% neq 0 (
-    echo sfc encountered an issue. Check the log for details.
+    echo sfc encountered an issue.
     pause
-    exit /b %errorlevel%
+    goto menu
 )
 echo Running DISM RestoreHealth...
 DISM /Online /Cleanup-Image /RestoreHealth
 if %errorlevel% neq 0 (
-    echo DISM RestoreHealth encountered an issue. Check the log for details.
+    echo DISM RestoreHealth encountered an issue.
     pause
-    exit /b %errorlevel%
+    goto menu
 )
 echo Done!
 pause
@@ -58,23 +75,23 @@ goto menu
 cls
 echo Downloading specific applications...
 echo Select an option:
-echo 1. vlc
+echo 1. VLC
 echo 2. Firefox
 echo 3. qBittorrent
-echo 4. neat
-echo 5. upgrade
+echo 4. Neat Download Manager
+echo 5. Upgrade all packages
 echo 6. Exit
 
-set /p choice=Enter your choice (1-6):
+set /p choice=Enter your choice (1-6): 
 
 if "%choice%"=="1" goto vlc
 if "%choice%"=="2" goto Firefox
 if "%choice%"=="3" goto qBittorrent
 if "%choice%"=="4" goto neat
 if "%choice%"=="5" goto upgrade
-if "%choice%"=="6" goto exit_script
+if "%choice%"=="6" goto menu
 echo Invalid choice. Please enter a number between 1 and 6.
-goto menu
+goto download_apps
 
 :upgrade
 cls
@@ -83,11 +100,12 @@ winget upgrade --all
 if %errorlevel% neq 0 (
     echo please go to https://winget.run/
     pause
-    exit /b %errorlevel%
+    goto menu
 )
 echo Done!
 pause
 goto menu
+
 :vlc
 cls
 echo Installing VLC...
@@ -95,11 +113,12 @@ winget install -e --id VideoLAN.VLC
 if %errorlevel% neq 0 (
     echo please go to https://www.videolan.org/vlc/
     pause
-    exit /b %errorlevel%
+    goto menu
 )
 echo Done!
 pause
 goto menu
+
 :Firefox
 cls
 echo Installing Firefox...
@@ -107,51 +126,45 @@ winget install -e --id Mozilla.Firefox
 if %errorlevel% neq 0 (
     echo please go to https://www.mozilla.org/en-US/firefox/new/
     pause
-    exit /b %errorlevel%
+    goto menu
 )
 echo Done!
 pause
 goto menu
+
 :qBittorrent
 cls
-echo Installing qbittorrent...
-
-:: Try to install using winget
+echo Installing qBittorrent...
 winget install -e --id qBittorrent.qBittorrent
 if %errorlevel% neq 0 (
     echo please go to https://www.qbittorrent.org/download
     pause
-    exit /b %errorlevel%
+    goto menu
 )
 echo Done!
 pause
 goto menu
+
 :neat
 cls
 echo Installing Neat Download Manager...
-
-:: Try to install using winget
 winget install -e --id JavadMotallebi.NeatDownloadManager
-
-:: Check if winget command was successful
 if %errorlevel% neq 0 (
     echo winget installation failed.
     echo Please go to https://www.neatdownloadmanager.com/index.php/en/
-    
-    :: Download using PowerShell
-    powershell -Command "Invoke-WebRequest -Uri 'https://www.neatdownloadmanager.com/file/NeatDM_setup.exe' -OutFile ([System.IO.Path]::Combine([System.Environment]::GetFolderPath('Desktop'), 'NeatDM_setup.exe'))"
-    
-    :: Install downloaded setup
-    echo Installing NeatDM_setup.exe...
+    powershell -Command "Invoke-WebRequest -Uri 'https://www.neatdownloadmanager.com/file/NeatDM_setup.exe' -OutFile ([System.IO.Path]::Combine([System.Environment]::GetFolderPath('Desktop'), 'CS Downloads', 'NeatDM_setup.exe'))"
     start /wait %USERPROFILE%\Desktop\NeatDM_setup.exe
-    
     echo Installation complete.
     pause
     goto menu
 )
+echo Done!
+pause
+goto menu
+
 :activate_windows
 cls
-echo Activating windows...
+echo Activating Windows...
 echo                             use PowerShell (Recommended)
 echo       1. Right-click on the Windows start menu and select PowerShell or Terminal (Not CMD).
 echo       2. Copy and paste the code below and press enter
@@ -164,25 +177,28 @@ echo Done!
 pause
 goto menu
 
-:playbook_and_ame
+:ame_playbook
 cls
 echo Downloading Atlas OS playbook...
-powershell -Command "Invoke-WebRequest -Uri 'https://github.com/Atlas-OS/Atlas/releases/download/0.4.0/AtlasPlaybook_v0.4.0.zip' -OutFile ([System.IO.Path]::Combine([System.Environment]::GetFolderPath('Desktop'), 'AtlasPlaybook_v0.4.0.zip'))"
+powershell -Command "Invoke-WebRequest -Uri 'https://github.com/Atlas-OS/Atlas/releases/download/0.4.0/AtlasPlaybook_v0.4.0.zip' -OutFile ([System.IO.Path]::Combine([System.Environment]::GetFolderPath('Desktop'), 'CS Downloads', 'AtlasPlaybook_v0.4.0.zip'))"
 if %errorlevel% neq 0 (
-    echo please go to https://atlasos.net/
-	start https://atlasos.net/
+    echo Failed to download Atlas OS playbook.
+    echo Please visit https://atlasos.net/
+    start https://atlasos.net/
     pause
-    exit /b %errorlevel%
+    goto menu
 )
-echo Downloading ame to Desktop / not working cuz of %20...
-powershell -Command "Invoke-WebRequest -Uri 'https://download.ameliorated.io/AME%20Wizard%20Beta.zip' -OutFile ([System.IO.Path]::Combine([System.Environment]::GetFolderPath('Desktop'), 'AME Wizard Beta.zip'))"
+echo Downloading AME Wizard to Desktop...
+powershell -Command "Invoke-WebRequest -Uri 'https://download.ameliorated.io/AME%20Wizard%20Beta.zip' -OutFile ([System.IO.Path]::Combine([System.Environment]::GetFolderPath('Desktop'), 'CS Downloads', 'AME Wizard Beta.zip'))"
 if %errorlevel% neq 0 (
-    echo please go to https://ameliorated.io/
-	start https://ameliorated.io/
+    echo Failed to download AME Wizard.
+    echo Please visit https://ameliorated.io/
+    start https://ameliorated.io/
     pause
-    exit /b %errorlevel%
+    goto menu
 )
-echo Done!
+
+echo Downloads completed successfully!
 pause
 goto menu
 
@@ -190,5 +206,4 @@ goto menu
 cls
 echo Exiting script.
 start https://catsmoker.github.io
-pause
 exit /b
